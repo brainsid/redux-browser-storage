df19251243b9b6a8e530dbe3dfdbd7b9b8ca16ed
Brian Sid <brainsid@netcraft.global>
2020-09-21 10:02:21 +0300

    Updates

---
+++ precache-manifest.983073fc8ee4b72e50e4a3bf3b6fe629.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "720fc58b00e88e66f9af66499ef21365",
+    "url": "/redux-browser-storage/index.html"
+  },
+  {
+    "revision": "f47ad8e688a8e4d3b12c",
+    "url": "/redux-browser-storage/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "72c6d56a9ba6b715c3d2",
+    "url": "/redux-browser-storage/static/js/2.8cd3c34b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-browser-storage/static/js/2.8cd3c34b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f47ad8e688a8e4d3b12c",
+    "url": "/redux-browser-storage/static/js/main.c8813c03.chunk.js"
+  },
+  {
+    "revision": "42a9f28af76595380d26",
+    "url": "/redux-browser-storage/static/js/runtime-main.e776ac34.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/redux-browser-storage/static/media/logo.5d5d9eef.svg"
+  }
+]);
