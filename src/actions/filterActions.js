ff0462b4e0bd12ff973546393e9909b486a785ec
Brian Sid <brainsid@netcraft.global>
2020-09-21 09:48:28 +0300

    nothing

--- src/actions/filterActions.js
+++ src/actions/filterActions.js
@@ -1,10 +1,5 @@
-import { ADD_FILTER } from "./types";
+import { ADD_FILTER, REMOVE_FILTER } from "./types";
 
-// export function addFilter() {
-//   return function (dispatch) {};
-// }
-
-// same with ES6
-export const addFilter = (filter) => (dispatch) => {
-  dispatch({ type: ADD_FILTER, payload: filter });
+export const addFilter = () => (dispatch) => {
+  dispatch({ type: ADD_FILTER });
 };
